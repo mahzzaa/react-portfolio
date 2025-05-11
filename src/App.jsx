ca131719ef628dca5cce4ab72a419781aa7c741a
mahzzaa <62835804+mahzzaa@users.noreply.github.com>
2025-05-11 18:06:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Home from "../src/pages/Home";
+import NotFound from "../src/pages/NotFound";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div>
+      {/* theme toggle */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
